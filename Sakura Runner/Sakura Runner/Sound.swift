3cdf6ecf8121d4a642d2598322d8259ca5c23fdb
jbird1998 <31713500+jbird1998@users.noreply.github.com>
2019-12-10 12:13:18 -0500

    Updated Sound.swift

--- Sakura Runner/Sakura Runner/Sound.swift
+++ Sakura Runner/Sakura Runner/Sound.swift
@@ -8,31 +8,31 @@
 import AVFoundation
 struct Sound {
     static var isVolumeOn = true
-    static var AudioPlayer = AVAudioPlayer()
-    static var jumpAudioPlayer = AVAudioPlayer()
-    static var slideAudioPlayer = AVAudioPlayer()
+    static var AudioPlayer: AVAudioPlayer?
+    static var jumpAudioPlayer: AVAudioPlayer?
+    static var slideAudioPlayer: AVAudioPlayer?
     static func playSound() {
         let AssortedMusics = NSURL(fileURLWithPath: Bundle.main.path(forResource: "music", ofType: "mp3")!)
         AudioPlayer = try! AVAudioPlayer(contentsOf: AssortedMusics as URL)
-        AudioPlayer.prepareToPlay()
-        AudioPlayer.numberOfLoops = -1
-        AudioPlayer.play()
+        AudioPlayer?.prepareToPlay()
+        AudioPlayer?.numberOfLoops = -1
+        AudioPlayer?.play()
     }
     static func stopSound() {
-        AudioPlayer.stop()
+        AudioPlayer?.stop()
     }
     static func playJumpSound() {
         let AssortedMusics = NSURL(fileURLWithPath: Bundle.main.path(forResource: "Jump", ofType: "mp3")!)
         jumpAudioPlayer = try! AVAudioPlayer(contentsOf: AssortedMusics as URL)
-        jumpAudioPlayer.prepareToPlay()
-        jumpAudioPlayer.numberOfLoops = 0
-        jumpAudioPlayer.play()
+        jumpAudioPlayer?.prepareToPlay()
+        jumpAudioPlayer?.numberOfLoops = 0
+        jumpAudioPlayer?.play()
     }
     static func playSlideSound() {
         let AssortedMusics = NSURL(fileURLWithPath: Bundle.main.path(forResource: "Slide", ofType: "mp3")!)
         slideAudioPlayer = try! AVAudioPlayer(contentsOf: AssortedMusics as URL)
-        slideAudioPlayer.prepareToPlay()
-        slideAudioPlayer.numberOfLoops = 0
-        slideAudioPlayer.play()
+        slideAudioPlayer?.prepareToPlay()
+        slideAudioPlayer?.numberOfLoops = 0
+        slideAudioPlayer?.play()
     }
 }
